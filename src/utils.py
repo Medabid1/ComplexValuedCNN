85167aa09989afc4f6c6591034b681a85871bbf9
Mohamed Abid <n.mohamedabid@gmail.com>
2019-08-19 13:23:48 -0400

    adding loss

--- src/utils.py
+++ src/utils.py
@@ -10,3 +10,9 @@ def concat(axis=-1, *args):
     x_i = torch.concat([j.i for j in args], axis=axis)
 
     return CPLX(x_r, x_i)
+
+def loss(labels, predictions, loss_function):
+    if isinstance(labels, CPLX):
+        return  loss_function(labels.r, predictions.r) + loss_function(labels.i, predictions.i)
+    else : 
+        raise 'Labels should be CPLX type'
