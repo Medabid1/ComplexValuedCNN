893038af25857baca6fcaa410c44799d885764eb
Mohamed Abid <n.mohamedabid@gmail.com>
2019-08-20 23:02:28 -0400

    cat

--- src/utils.py
+++ src/utils.py
@@ -6,8 +6,8 @@ from src.complex import CPLX
 
 def concat(axis=-1, *args):
 
-    x_r = torch.concat([j.r for j in args], axis=axis)
-    x_i = torch.concat([j.i for j in args], axis=axis)
+    x_r = torch.cat([j.r for j in args], axis=axis)
+    x_i = torch.cat([j.i for j in args], axis=axis)
 
     return CPLX(x_r, x_i)
 
